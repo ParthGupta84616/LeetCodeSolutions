3800adfe519b17322f2007023402b20444b7558c
Parth Gupta <148619447+ParthGupta84616@users.noreply.github.com>
2025-08-15 20:24:42 +0530

    Time: 0 ms (100%), Space: 8 MB (18.14%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-
-        string binary = bitset<32>(n).to_string();
-        int count = 0;
-        int idx = 0;
-        for (int i = 0; i < 32; i++) {
-            if (binary[i] == '1'){
-                count++;
-                
-                idx = i;
-            }
-            
-
+       if(n<=0) return false;
+        while (n % 4 == 0) {
+            n /= 4;
         }
-        return (count == 1 && idx%2 == 1 ) ? true : false;
+        return n == 1;
     }
 };
