353ea70b3f1ab67ef391f5611dba3e4dd5b6d927
Parth Gupta <148619447+ParthGupta84616@users.noreply.github.com>
2025-10-27 16:24:22 +0530

    Time: 8 ms (48.88%), Space: 27.2 MB (84.17%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        vector<int> res;
+        // vector<int> res;
+        int lastLayer = 0;
+        long long output = 0;
         for (int i = 0; i < bank.size(); i++) {
             int layerSum = 0;
             for (int j = 0; j < bank[0].length(); j++) {
@@ -10,16 +12,13 @@ public:
                 }
             }
             if (layerSum > 0) {
-                res.push_back(layerSum);
+                if (lastLayer == 0) {
+                    lastLayer = layerSum;
+                } else {
+                    output += layerSum * lastLayer;
+                    lastLayer = layerSum;
+                }
             }
-        }
-        long long output = 0;
-        if(res.size() < 2){
-            return 0;
-        }
-        for(int i = 0 ; i < res.size() - 1 ; i++){
-            cout<< res[i + 1];
-            output += res[i] * res[i+1];
         }
         return output;
     }
